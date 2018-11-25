1877ccfdcb70616f5142a43f496e9b808f595ff7
zeb <2296418129@qq.com>
2018-11-25 17:42:09 +0800

    01

---
+++ 01.js
@@ -0,0 +1,14 @@
+function read01(){
+	require('fs').readFile('D:/SVN/tempJs/my.txt',function(err,data){
+		console.log(data.toString());
+	})
+}
+function test01(){
+	var sum = 0;
+	for(var i=1;i<101;i++){
+		sum = sum + i;
+	}
+	console.log('1+2+3....+100='+sum);
+}
+read01();
+test01();
