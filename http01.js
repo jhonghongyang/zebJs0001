b1fba6affca4f83c8e0cd866db260c75a1032458
zeb <2296418129@qq.com>
2018-11-25 21:33:22 +0800

    02

--- http01.js
+++ http01.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var server01 = ()=>{
     require('http').createServer(function(req,res){
-        //res.setHeader('Content-Type','text/html;charset=utf-8')
-        //res.end('<h1>老子天下第一000'+req.connection.remoteAddress+'</h1>')
+        res.setHeader('Content-Type','text/html;charset=utf-8')
+        res.write('<h1>老子天下第一000'+req.connection.remoteAddress+'</h1>')
         fs.readFile('D:\\SVN\\tempJs\\my.txt',function(err,data){
             console.log(data.toString());
             res.end('<h2>'+data.toString()+'</h2>');
